12b3af5b488bdccdfee541a77ad9e311ad39bf53
karamchand11 <0215k.c@gmail.com>
2017-01-11 14:21:40 +0000

    karam.sh

--- a.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-a=3
-if [ $a -gt 2 ]
-then
-echo "A is grater"
-else
-echo "A is lessthan 3"
-exit 1
-fi
--- c.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-c=40
-if [ $c -gt 30 ]
-then
-echo "c is grater"
-else
-echo "c is lessthan 30"
-exit 1
-fi
